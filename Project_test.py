ffef206e198c74e8c1993745161cd3a89956a31f
ElenaRomashkova <elen.iva89@gmail.com>
2017-05-08 20:55:00 +0300

    фмнансовый расчет

--- Project_test.py
+++ Project_test.py
@@ -1,1 +1,5 @@
-print("Hello world")
+car=int(input("Обслуживание машины"))
+rent=int(input("Аренда квартиры"))
+food=int(input("Обеды и ужины в ресторанах"))
+total=car+rent+food
+print("\n Общая сумма:", total)
